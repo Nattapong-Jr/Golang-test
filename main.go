d9aca4ad9e23b5607e6550e2b514eb904b447e27
Nattapong-Jr <68030135@kmitl.ac.th>
2026-09-12 19:45:05 +0700

    try valiables

--- main.go
+++ main.go
@@ -2,10 +2,23 @@ package main
 
 import (
 	"fmt"
-	"rsc.io/quote"
 )
 
-func main(){
-	fmt.Println("Hello World")
-	fmt.Println(quote.Go())
-} 	
+var number,number2 int = 1000, 2000
+var msg string = "Hello"
+
+func main() {
+
+	numberfloat := 34.4
+	fmt.Println("Hello, Son!")
+	fmt.Println("The number is:", number)
+	fmt.Println(number)
+	fmt.Println(number2)
+	fmt.Println(numberfloat)
+	fmt.Println(msg)
+
+	fmt.Println(number + number2)
+	fmt.Println(float64(number + number2) + numberfloat)
+	fmt.Println(msg + "World")
+	fmt.Println("My money = ",number2)
+}
